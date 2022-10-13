96d51b2956659693b87933388b428d1d921dbc28
Walter Reiner <walter.reiner@wreiner.at>
2022-10-13 10:22:11 +0200

    Move conversion init to class init, add logging and debug flag

--- epc.py
+++ epc.py
@@ -1,6 +1,8 @@
 import argparse
 import json
+import logging
 from operator import mul, truediv
+import sys
 
 CONVERSION_TABLE = {
     "kWh2Wh": {"operator": "*", "factor": 1000},
@@ -24,6 +26,9 @@ class EnergyProfileConverter:
         self.initialize()
 
     def initialize(self):
+        # setup logging
+        self.setup_logging(self.args["debug"])
+
         self.load_json_data(self.args["in"][0])
         self.output_dict = self.initialize_output_dict(self.original_data)
 
@@ -37,13 +42,24 @@ class EnergyProfileConverter:
             self.start_interval, self.convert_interval
         )
 
+        # read the conversion operator and factor once as it is static for all
+        # conversion operations
+        conversionkey = self.from_unit + "2" + self.to_unit
+        self.conversion_operator = CONVERSION_TABLE[conversionkey]["operator"]
+        self.conversion_factor = CONVERSION_TABLE[conversionkey]["factor"]
+
+    def setup_logging(self, debug):
+        if debug:
+            logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+        else:
+            logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+
     def parse_commandline_arguments(self):
         parser = argparse.ArgumentParser()
 
         parser._action_groups.pop()
         required = parser.add_argument_group("required arguments")
-        # if needed
-        # optional = parser.add_argument_group('optional arguments')
+        optional = parser.add_argument_group("optional arguments")
 
         # use if -in and -out need to be positional arguments
         # add validation for positional arguments if needed
@@ -82,23 +98,27 @@ class EnergyProfileConverter:
             required=True,
         )
 
+        optional.add_argument(
+            "--debug", action="store_true", help="Print debug messages to stderr"
+        )
+
         # convert args to dict
         self.args = vars(parser.parse_args())
 
     def load_json_data(self, filename):
-        print(f"will read JSON from {filename} ..")
+        logging.info(f"will read JSON from {filename} ..")
 
         with open(filename) as f:
             self.original_data = json.load(f)
 
     def write_json_data(self, filename):
-        print(f"will write output JSON to {filename}")
+        logging.info(f"will write output JSON to {filename}")
 
         with open(filename, "w") as f:
             json.dump(self.output_dict, f, indent=4)
 
     def initialize_output_dict(self, original_data):
-        print("will initialize output JSON data structure ..")
+        logging.info("will initialize output JSON data structure ..")
 
         output_dict = dict()
         # duplicate header information
@@ -128,7 +148,7 @@ class EnergyProfileConverter:
             yield iterable generator
         """
         if len(data_array) < window_size:
-            print("error, window size bigger than data array")
+            logging.error("error, window size bigger than data array")
             return None
 
         window_position = 0
@@ -165,10 +185,10 @@ class EnergyProfileConverter:
         # calculate window_size or padding
         if start_interval < convert_interval:
             self.window_size = int(convert_interval / start_interval)
-            print(f"Window size: {self.window_size}")
+            logging.debug(f"Window size: {self.window_size}")
         else:
             self.padding = int(start_interval / convert_interval)
-            print(f"Padding size: {self.padding}")
+            logging.debug(f"Padding size: {self.padding}")
 
     def convert_unit(self, value):
         """
@@ -183,23 +203,29 @@ class EnergyProfileConverter:
             Converted value
         """
         if self.from_unit == self.to_unit:
-            print(
+            logging.info(
                 f"units from {self.from_unit} to {self.to_unit} are equal, will not convert .."
             )
             return value
 
-        print(f"will convert value {value} from {self.from_unit} to {self.to_unit} ..")
+        logging.debug(
+            f"will convert value {value} from {self.from_unit} to {self.to_unit} .."
+        )
 
-        conversionkey = self.from_unit + "2" + self.to_unit
-        operator = CONVERSION_TABLE[conversionkey]["operator"]
-        factor = CONVERSION_TABLE[conversionkey]["factor"]
+        # move conversion operator and factor initialization to class init
+        # as those values are static for all operations
+        # conversionkey = self.from_unit + "2" + self.to_unit
+        # operator = CONVERSION_TABLE[conversionkey]["operator"]
+        # factor = CONVERSION_TABLE[conversionkey]["factor"]
 
-        if operator == "/":
-            return truediv(value, factor)
-        elif operator == "*":
-            return mul(value, factor)
+        if self.conversion_operator == "/":
+            return truediv(value, self.conversion_factor)
+        elif self.conversion_operator == "*":
+            return mul(value, self.conversion_factor)
         else:
-            print(f"ERROR: unknown operator {operator} found in conversion table")
+            logging.error(
+                f"ERROR: unknown operator {self.conversion_operator} found in conversion table"
+            )
             return None
 
     def convert(self):
@@ -233,18 +259,21 @@ class EnergyProfileConverter:
             if self.start_interval < self.convert_interval:
                 average = sum(window_elements) / self.window_size
                 converted_average = self.convert_unit(average)
-                # print(f"converted unit from {average}{self.from_unit} to {converted_average}{self.to_unit}")
+                logging.debug(
+                    f"converted unit from {average}{self.from_unit} to {converted_average}{self.to_unit}"
+                )
                 self.output_dict["data"].append(converted_average)
             else:
                 # add n elements same as current element
                 single_element = self.convert_unit(window_elements[0])
-                # print(f"converted unit from {window_elements[0]}{self.from_unit} to {single_element}{self.to_unit}")
+                logging.debug(
+                    f"converted unit from {window_elements[0]}{self.from_unit} to {single_element}{self.to_unit}"
+                )
 
                 self.output_dict["data"].extend(
                     [single_element for v in range(0, self.padding)]
                 )
 
-        # print(self.output_dict)
         self.write_json_data(self.args["out"][0])
 
 
